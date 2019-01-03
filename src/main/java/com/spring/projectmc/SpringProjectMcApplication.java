22c85c39dbfc112d8e2277d298f666401912ae5b
Rodrik Klesse <rodrik_hsk@hotmail.com>
2019-01-03 14:01:31 -0200

    Project using Spring Boot was created

---
+++ src/main/java/com/spring/projectmc/SpringProjectMcApplication.java
@@ -0,0 +1,14 @@
+package com.spring.projectmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectMcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectMcApplication.class, args);
+	}
+
+}
+
