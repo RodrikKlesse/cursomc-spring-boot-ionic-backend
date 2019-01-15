f0fcb52919013b5fc326dc81bfff061f7c65703e
Rodrik Klesse <rodrik_hsk@hotmail.com>
2019-01-14 22:39:00 -0200

    Enable Endpoint /clientes/{id}

---
+++ src/main/java/com/spring/projectmc/services/ClienteService.java
@@ -0,0 +1,26 @@
+package com.spring.projectmc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.spring.projectmc.domain.Cliente;
+import com.spring.projectmc.repositories.ClienteRepository;
+import com.spring.projectmc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		//Cliente obj = repo.findOne(id);
+		//return obj;
+		
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
