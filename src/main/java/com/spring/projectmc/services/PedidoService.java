dc61b16986db12f0bc233832cca2e054715c93b7
Rodrik Klesse <rodrik_hsk@hotmail.com>
2019-01-15 00:28:06 -0200

    Enable Endpoint /pedidos/{id}

---
+++ src/main/java/com/spring/projectmc/services/PedidoService.java
@@ -0,0 +1,26 @@
+package com.spring.projectmc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.spring.projectmc.domain.Pedido;
+import com.spring.projectmc.repositories.PedidoRepository;
+import com.spring.projectmc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		//Pedido obj = repo.findOne(id);
+		//return obj;
+		
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
